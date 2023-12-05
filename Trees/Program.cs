00c57c888a27f40c3974865e28d7b5fd2f7d6ac7
Devo2003 <93745734+Devo2003@users.noreply.github.com>
2023-12-05 15:44:44 -0600

    added in methods to read and sort the text file in a bubble sort algorithm. Next up is making the tree stucture

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics;
+
+namespace Trees
+{
+    class Program
+    {
+        static void Main()
+        {
+            createTree ct = new createTree();
+
+            string fileDirectory = "/Users/devinsalgado35/Desktop/TreeAlgorithm/Trees/scores.txt";
+            int[] data = ct.fileCovertToArray(fileDirectory);
+
+
+            ct.BubbleSort(data);
+            Console.ReadKey();
+        }
+    }
+}
 
---
+++ Trees/createTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace Trees
+{
+    public class createTree
+    {
+        public int[] Nums;
+        public string[] fileLines;
+        public string fileDirectory;
+
+        public createTree()
+        {
+            //this line gives in the path of the file into a string
+            fileDirectory = "/Users/devinsalgado35/Desktop/TreeAlgorithm/Trees/scores.txt";
+            Nums = fileCovertToArray(fileDirectory);
+        }
+
+        public int[] fileCovertToArray(string fileDirectory)
+        {
+            fileLines = File.ReadAllLines(fileDirectory);
+
+            Nums = new int[fileLines.Length];
+            
+            for (int i = 0; i < fileLines.Length; i++)
+            {
+                if (int.TryParse(fileLines[i], out int value))
+                {
+                    Nums[i] = value;
+                }
+            }
+            return Nums;
+        }
+
+        public void printArray(int[] Nums)
+        {
+            foreach (var item in Nums)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+
+        // Name: BubbleSort Algorithm
+        // Description:
+        // O(n^2) average and worst case
+
+        public void BubbleSort(int[] data)
+        {
+            for (int i = 0; i < Nums.Length; i++)
+            {
+                for (int j = 0; j < Nums.Length - 1; j++)
+                {
+                    if (Nums[j] > Nums[j + 1])
+                    {
+                        int temp = Nums[j];
+                        Nums[j] = Nums[j + 1];
+                        Nums[j + 1] = temp;
+                        //printArray(Nums);
+
+                    }
+                }
+            }
+            printArray(Nums);
+        }
+
+
+    }
+}
+
+
